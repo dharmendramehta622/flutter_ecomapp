e07deba4f82ac1cd9c7f0e5ac5302786ff39cdbc
kodemafia <dharmendramehta622@gmail.com>
2024-08-06 19:01:55 +0545

    loading issue solve

--- lib/Networks/models/clockin_model.dart
+++ lib/Networks/models/clockin_model.dart
@@ -65,70 +65,151 @@ class ClockInReport {
 }
 
 class ClockInModel {
-  final int? id;
-  final DateTime? createdAt;
-  final DateTime? checkIn;
-  final DateTime? checkOut;
-  final String? lat;
-  final String? long;
-  final String? user;
+    final int? id;
+    final User? user;
+    final DateTime? createdAt;
+    final DateTime? checkIn;
+    final DateTime? checkOut;
+    final String? lat;
+    final String? long;
 
-  ClockInModel({
-    this.id,
-    this.createdAt,
-    this.checkIn,
-    this.checkOut,
-    this.lat,
-    this.long,
-    this.user,
-  });
+    ClockInModel({
+        this.id,
+        this.user,
+        this.createdAt,
+        this.checkIn,
+        this.checkOut,
+        this.lat,
+        this.long,
+    });
 
-  ClockInModel copyWith({
-    int? id,
-    DateTime? createdAt,
-    DateTime? checkIn,
-    DateTime? checkOut,
-    String? lat,
-    String? long,
-    String? user,
-  }) =>
-      ClockInModel(
-        id: id ?? this.id,
-        createdAt: createdAt ?? this.createdAt,
-        checkIn: checkIn ?? this.checkIn,
-        checkOut: checkOut ?? this.checkOut,
-        lat: lat ?? this.lat,
-        long: long ?? this.long,
-        user: user ?? this.user,
-      );
+    ClockInModel copyWith({
+        int? id,
+        User? user,
+        DateTime? createdAt,
+        DateTime? checkIn,
+        DateTime? checkOut,
+        String? lat,
+        String? long,
+    }) => 
+        ClockInModel(
+            id: id ?? this.id,
+            user: user ?? this.user,
+            createdAt: createdAt ?? this.createdAt,
+            checkIn: checkIn ?? this.checkIn,
+            checkOut: checkOut ?? this.checkOut,
+            lat: lat ?? this.lat,
+            long: long ?? this.long,
+        );
 
-  factory ClockInModel.fromJson(String str) =>
-      ClockInModel.fromMap(json.decode(str));
+    factory ClockInModel.fromJson(String str) => ClockInModel.fromMap(json.decode(str));
 
-  String toJson() => json.encode(toMap());
+    String toJson() => json.encode(toMap());
 
-  factory ClockInModel.fromMap(Map<String, dynamic> json) => ClockInModel(
+    factory ClockInModel.fromMap(Map<String, dynamic> json) => ClockInModel(
         id: json["id"],
-        createdAt: json["created_at"] == null
-            ? null
-            : DateTime.parse(json["created_at"]),
-        checkIn:
-            json["check_in"] == null ? null : DateTime.parse(json["check_in"]),
-        checkOut: json["check_out"] == null
-            ? null
-            : DateTime.parse(json["check_out"]),
+        user: json["user"] == null ? null : User.fromMap(json["user"]),
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        checkIn: json["check_in"] == null ? null : DateTime.parse(json["check_in"]),
+        checkOut: json["check_out"] == null ? null : DateTime.parse(json["check_out"]),
         lat: json["lat"],
         long: json["long"],
-        user: json["user"],
-      );
+    );
 
-  Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toMap() => {
         "id": id,
+        "user": user?.toMap(),
         "created_at": createdAt?.toIso8601String(),
         "check_in": checkIn?.toIso8601String(),
         "check_out": checkOut?.toIso8601String(),
         "lat": lat,
         "long": long,
-        "user": user,
-      };
+    };
+}
+
+class User {
+    final String? id;
+    final String? role;
+    final String? email;
+    final String? phoneNo;
+    final String? firstName;
+    final String? lastName;
+    final dynamic userImage;
+    final bool? isActive;
+    final DateTime? dateJoined;
+    final DateTime? lastLogin;
+    final bool? emailVerified;
+
+    User({
+        this.id,
+        this.role,
+        this.email,
+        this.phoneNo,
+        this.firstName,
+        this.lastName,
+        this.userImage,
+        this.isActive,
+        this.dateJoined,
+        this.lastLogin,
+        this.emailVerified,
+    });
+
+    User copyWith({
+        String? id,
+        String? role,
+        String? email,
+        String? phoneNo,
+        String? firstName,
+        String? lastName,
+        dynamic userImage,
+        bool? isActive,
+        DateTime? dateJoined,
+        DateTime? lastLogin,
+        bool? emailVerified,
+    }) => 
+        User(
+            id: id ?? this.id,
+            role: role ?? this.role,
+            email: email ?? this.email,
+            phoneNo: phoneNo ?? this.phoneNo,
+            firstName: firstName ?? this.firstName,
+            lastName: lastName ?? this.lastName,
+            userImage: userImage ?? this.userImage,
+            isActive: isActive ?? this.isActive,
+            dateJoined: dateJoined ?? this.dateJoined,
+            lastLogin: lastLogin ?? this.lastLogin,
+            emailVerified: emailVerified ?? this.emailVerified,
+        );
+
+    factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        role: json["role"],
+        email: json["email"],
+        phoneNo: json["phone_no"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        userImage: json["user_image"],
+        isActive: json["is_active"],
+        dateJoined: json["date_joined"] == null ? null : DateTime.parse(json["date_joined"]),
+        lastLogin: json["last_login"] == null ? null : DateTime.parse(json["last_login"]),
+        emailVerified: json["email_verified"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "role": role,
+        "email": email,
+        "phone_no": phoneNo,
+        "first_name": firstName,
+        "last_name": lastName,
+        "user_image": userImage,
+        "is_active": isActive,
+        "date_joined": dateJoined?.toIso8601String(),
+        "last_login": lastLogin?.toIso8601String(),
+        "email_verified": emailVerified,
+    };
 }
